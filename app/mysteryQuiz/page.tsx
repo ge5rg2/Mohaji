db556d8ff9e15ad8f4758e0a5270f843923159c0
ge5rg2 <kghee9612@gmail.com>
2023-05-31 01:16:52 +0900

    #2.1-set MysteryQuizanswer

--- app/mysteryQuiz/page.tsx
+++ app/mysteryQuiz/page.tsx
@@ -2,11 +2,43 @@
 
 import React, { useRef, useState } from 'react';
 
-export default function Gpt() {
-  const [quizNum, setQuizNum] = useState<number>();
-  const containerRef = useRef<any>(null);
+interface numSeq {
+  num: number;
+  seq: string;
+}
 
-  const onTest = async () => {
+export default function Mystery() {
+  const [quizNum, setQuizNum] = useState<numSeq>({
+    num: 0,
+    seq: '',
+  });
+  const containerRef = useRef<any>(null);
+  const containerAnswerRef = useRef<any>(null);
+
+  const onQuizAnswer = async () => {
+    try {
+      const response = await fetch('/api/mysteryQuiz', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ num: quizNum.num, seq: quizNum.seq }),
+      });
+      const data = await response.json();
+      if (response.status !== 200) {
+        throw data.error || new Error(`request failed with status ${response.status}`);
+      } else {
+        if (containerAnswerRef.current) {
+          const containerAnswer = containerAnswerRef.current;
+          containerAnswer.innerHTML = data.result;
+        }
+      }
+    } catch (error: unknown) {
+      console.log(error);
+    }
+  };
+
+  const onSearchQuiz = async () => {
     try {
       const response = await fetch('/api/mysteryQuiz', {
         method: 'GET',
@@ -18,13 +50,13 @@ export default function Gpt() {
       if (response.status !== 200) {
         throw data.error || new Error(`request failed with status ${response.status}`);
       } else {
-        setQuizNum(data.num);
+        setQuizNum({ num: data.num, seq: data.seq });
         if (containerRef.current) {
           const container = containerRef.current;
           container.innerHTML = data.result;
         }
       }
-    } catch (error) {
+    } catch (error: unknown) {
       console.log(error);
     }
   };
@@ -33,10 +65,17 @@ export default function Gpt() {
     <main className="flex flex-col items-center">
       <div>Mystery Quiz🕵️</div>
       <div>
-        <button onClick={onTest}>Start</button>
+        <button className="bg-gray-500 hover:bg-gray-600 custom-button" onClick={onSearchQuiz}>
+          Start
+        </button>
       </div>
-      <div>{quizNum}</div>
-      <div className="flex flex-col" ref={containerRef} />
+      <div className="flex flex-col p-10">
+        <div ref={containerRef} />
+        <button onClick={onQuizAnswer} className="bg-gray-500 hover:bg-gray-600 custom-button">
+          Answer
+        </button>
+        <div ref={containerAnswerRef} />
+      </div>
     </main>
   );
 }
--- pages/api/mysteryQuiz.ts
+++ pages/api/mysteryQuiz.ts
@@ -1,38 +1,63 @@
-import axios, { AxiosResponse, AxiosError } from 'axios';
+import axios from 'axios';
 import { NextApiRequest, NextApiResponse } from 'next';
 const cheerio = require('cheerio');
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-  const randomQuiz = Math.floor(Math.random() * (3650 - 3200 + 1)) + 3200;
-  if (req.method !== 'GET') {
-    return res.status(400).json('This is an incorrect approach.');
+
+  if (req.method === 'GET') {
+    try {
+      const randomQuiz = Math.floor(Math.random() * (604 - 250 + 1)) + 250;
+      const listResponse = await axios.get(
+        `https://www.nis.go.kr:4016/CM/1_5_1/list.do?selectBox=1&fromDate=&toDate=&searchKeyword=${randomQuiz}`,
+      );
+      const $ = cheerio.load(listResponse.data);
+      const qNum = $('#board > tbody > tr').attr('id');
+
+      if (!qNum) {
+        console.log('No match found');
+        return res.status(400).json('No match found');
+      }
+
+      const viewResponse = await axios.get(
+        `https://www.nis.go.kr:4016/CM/1_5_1/view.do?seq=${qNum}&currentPage=1&selectBox=&searchKeyword=&fromDate=&toDate=`,
+      );
+      const sourceData = $(
+        '#selectListForm > div > div.board-detail > div.board-content.txt-spo-body-2-400.font-gray01',
+        viewResponse.data,
+      ).html();
+
+      const secondListResponse = await axios.get(
+        `https://www.nis.go.kr:4016/CM/1_5_2/list.do?selectBox=1&fromDate=&toDate=&searchKeyword=${randomQuiz}`,
+      );
+      const seqNum = cheerio.load(secondListResponse.data)('#board > tbody > tr').attr('id');
+
+      if (!seqNum) {
+        console.log('No match found');
+        return res.status(400).json('No match found');
+      }
+
+      return res.status(200).json({ result: sourceData, num: qNum, seq: seqNum });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json('Internal Server Error');
+    }
+  } else if (req.method === 'POST') {
+    try {
+      const { num, seq } = req.body;
+      const viewResponse = await axios.get(
+        `https://www.nis.go.kr:4016/CM/1_5_2/view.do?seq=${seq}&currentPage=1&quizNum=${num}&selectBox=&searchKeyword=&fromDate=&toDate=`,
+      );
+      const $ = cheerio.load(viewResponse.data);
+      const answerData = $(
+        '#selectListForm > div > div.board-detail > div.board-content.answer-content.txt-spo-body-2-400.font-gray01 > p:nth-child(3)',
+      ).html();
+      return res.status(200).json({ result: answerData });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json('Internal Server Error');
+    }
   } else {
-    axios({
-      // 크롤링을 원하는 페이지 URL
-      url: `https://www.nis.go.kr:4016/CM/1_5_1/view.do?seq=${randomQuiz}&currentPage=52&selectBox=0&searchKeyword=&fromDate=&toDate=`,
-      method: 'GET',
-    })
-      .then((response: AxiosResponse) => {
-        const $ = cheerio.load(response.data);
-        const sourceData = $(
-          '#selectListForm > div > div.board-detail > div.board-content.txt-spo-body-2-400.font-gray01',
-        ).html();
-        console.log(sourceData);
-        return res.status(200).json({ result: sourceData, num: randomQuiz });
-      })
-      .catch((err: AxiosError) => {
-        return console.error(err);
-      });
+    return res.status(400).json('This is an incorrect approach.');
   }
 }
-// 3200 -
-// https://www.nis.go.kr:4016/CM/1_5_1/view.do?seq=3664&currentPage=1&selectBox=&searchKeyword=&fromDate=&toDate= -> 문제
-
-// https://www.nis.go.kr:4016/CM/1_5_1/view.do?seq=3149&currentPage=1&selectBox=1&searchKeyword=400&fromDate=&toDate= -> 400번 고정
-// https://www.nis.go.kr:4016/CM/1_5_2/view.do?seq=429645&currentPage=1&quizNum=3149&selectBox=1&searchKeyword=400&fromDate=&toDate=
-
-// https://www.nis.go.kr:4016/CM/1_5_2/view.do?seq=551807&currentPage=1&quizNum=3664&selectBox=&searchKeyword=&fromDate=&toDate=
-// https://www.nis.go.kr:4016/CM/1_5_2/view.do?seq=550847&currentPage=1&quizNum=3663&selectBox=&searchKeyword=&fromDate=&toDate=
-// https://www.nis.go.kr:4016/CM/1_5_2/view.do?seq=550236&currentPage=1&quizNum=3662&selectBox=&searchKeyword=&fromDate=&toDate=
-// #\33 664 > td.border-gray04.row-title > a
