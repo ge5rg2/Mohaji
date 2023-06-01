7b82ed8a0d730e24fad7debed18640c5c62eea89
ge5rg2 <kghee9612@gmail.com>
2023-06-01 22:20:09 +0900

    test to html head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 //import { Inter } from 'next/font/google';
 import './styles/globals.css';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export const metadata = {
   title: 'Mohaji',
@@ -10,6 +11,12 @@ export const metadata = {
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no, maximum-scale=1, width=device-width" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+      </Head>
       <body>
         <div className="navbar">
           <Link href="/baseball">⚾</Link>
