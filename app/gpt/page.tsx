3160feedba6d1c0c4b85da2a944321daf8d9a8fa
ge5rg2 <kghee9612@gmail.com>
2023-06-16 18:54:35 +0900

    feat: add current token

--- app/gpt/page.tsx
+++ app/gpt/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Questions from 'components/Questions';
 import Emoji from 'components/Emoji';
 import Word from 'components/Word';
+import { getSession } from 'next-auth/react';
+import { ResultData, userType } from '../interface/types';
 
 export default function Gpt() {
   const [questions, setQuestions] = useState<boolean>(false);
@@ -11,7 +13,8 @@ export default function Gpt() {
   const [word, setWord] = useState<boolean>(false);
   const [selected, setSelected] = useState<boolean>(false);
   const [currentType, setCurrentType] = useState<string>('');
-
+  const [resultData, setResultData] = useState<ResultData | null>(null);
+  const [userInfo, setUserInfo] = useState<userType | null>(null);
   /** GPT 선택한 컴포넌트 호출
    * DB에 사용가능한 토큰 수를 각 게임마다 할당.  사용시 토큰 개수 차감
    */
@@ -49,6 +52,26 @@ export default function Gpt() {
     return setSelected((pre) => !pre);
   };
 
+  useEffect(() => {
+    const fetchSession = async () => {
+      const session = await getSession();
+      const response = await fetch(`/api/userInfo?email=${session?.user?.email}`, { method: 'GET' });
+      const result = await response.json();
+      setResultData(result);
+      console.log(result);
+      if (session?.user) {
+        const userInfo: userType = {
+          name: session.user.name || '',
+          email: session.user.email || '',
+          image: session.user.image || '',
+        };
+        setUserInfo(userInfo);
+      }
+    };
+
+    fetchSession();
+  }, []);
+
   return (
     <main className="flex flex-col items-center">
       {selected ? (
@@ -68,23 +91,44 @@ export default function Gpt() {
         <>
           <div className="font-bold">다음 목록 중 하나를 선택하세요</div>
           <div className="flex items-center">
-            <button className="custom-button bg-gray-500 hover:bg-gray-600" name="word" onClick={(e) => onPressType(e)}>
-              끝말잇기
-            </button>
-            <button className="custom-button bg-gray-500 hover:bg-gray-600" name="que" onClick={(e) => onPressType(e)}>
-              10 Q&A
-            </button>
-            <button
-              className="custom-button bg-gray-500 hover:bg-gray-600"
-              name="emoji"
-              onClick={(e) => onPressType(e)}>
-              Emoji 변환기
-            </button>
+            <div className="flex-col ">
+              <div>
+                <button
+                  className="custom-button bg-gray-500 hover:bg-gray-600"
+                  name="word"
+                  onClick={(e) => onPressType(e)}>
+                  끝말잇기
+                </button>
+              </div>
+              <span className="text-center block mt-1">🪙{resultData?.token.word}</span>
+            </div>
+            <div className="flex-col ">
+              <div>
+                <button
+                  className="custom-button bg-gray-500 hover:bg-gray-600"
+                  name="que"
+                  onClick={(e) => onPressType(e)}>
+                  10 Q&A
+                </button>
+              </div>
+              <span className="text-center block mt-1">🪙{resultData?.token.quiz}</span>
+            </div>
+            <div className="flex-col ">
+              <div>
+                <button
+                  className="custom-button bg-gray-500 hover:bg-gray-600"
+                  name="emoji"
+                  onClick={(e) => onPressType(e)}>
+                  Emoji 변환기
+                </button>
+              </div>
+              <span className="text-center block mt-1">🪙{resultData?.token.emoji}</span>
+            </div>
           </div>
         </>
       )}
       {questions ? <Questions /> : ''}
-      {emoji ? <Emoji /> : ''}
+      {emoji && userInfo ? <Emoji /> : ''}
       {word ? <Word /> : ''}
     </main>
   );
