c713f6b0e7a59494b5e8a4ffc2edf521d6e63d28
ge5rg2 <kghee9612@gmail.com>
2023-06-16 18:56:44 +0900

    feat: add whole interface type

---
+++ app/interface/types.ts
@@ -0,0 +1,14 @@
+export interface ResultData {
+  token: {
+    quiz: number;
+    emoji: number;
+    word: number;
+  };
+  setTokenTime: string;
+}
+
+export interface userType {
+  name: string;
+  email: string;
+  image: string;
+}
