bcb1957800af6514596a6a28db85933a76f5bfd9
ge5rg2 <kghee9612@gmail.com>
2023-06-16 18:55:49 +0900

    feat: decrease dmoji token logic

--- pages/api/gpt/emoji.ts
+++ pages/api/gpt/emoji.ts
@@ -1,10 +1,14 @@
 import * as dotenv from 'dotenv';
 import { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import { authOptions } from '../auth/[...nextauth]';
+import { connectDB } from 'util/database';
+import { getServerSession } from 'next-auth';
 
 dotenv.config({ path: __dirname + '/.env' });
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const session = await getServerSession(req, res, authOptions);
   const { value } = req.body || '';
 
   try {
@@ -28,9 +32,11 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         },
       },
     );
-
-    console.log(response.data);
-    res.status(200).json({ result: response.data });
+    const db = (await connectDB).db('test');
+    const decreaseToken = await db
+      .collection('users')
+      .findOneAndUpdate({ email: session?.user?.email }, { $inc: { 'gptToken.emoji': -1 } });
+    res.status(200).json({ result: response.data, token: decreaseToken.ok });
   } catch (error) {
     console.log(error);
     return res.status(500);
