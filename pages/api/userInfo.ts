a63d0781d201cec79442bf53789099a17ff0fc5b
ge5rg2 <kghee9612@gmail.com>
2023-06-19 19:00:26 +0900

    feat: rest tokens to 10 if the Token time is more than 24H ago

--- pages/api/userInfo.ts
+++ pages/api/userInfo.ts
@@ -9,6 +9,26 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const db = client.db('test');
     const { email } = req.query;
     const result = await db.collection('users').findOne({ email });
+    if (result?.setTokenTime) {
+      const currentTime = new Date();
+      const tokenTime = new Date(result.setTokenTime);
+      const timeDiff = currentTime.getTime() - tokenTime.getTime();
+      const twentyFourHours = 24 * 60 * 60 * 1000; // 24 hours in milliseconds
+
+      if (timeDiff >= twentyFourHours) {
+        await db.collection('users').findOneAndUpdate(
+          { email },
+          {
+            $set: {
+              'gptToken.emoji': 10,
+              'gptToken.quiz': 10,
+              'gptToken.word': 10,
+              setTokenTime: currentTime.toISOString(), // Update setTokenTime to current time
+            },
+          },
+        );
+      }
+    }
     return res.status(200).json({
       token: result?.gptToken,
       setTokenTime: result?.setTokenTime,
